1507a8f8d68a12094cf551bc3ad987d028a350a0
git9527 <398816613@qq.com>
2022-04-05 19:22:36 +0800

    use baidu tongji

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
-import uweb from 'vue-uweb'
+import ba from 'vue-ba'
 
 import VueKonva from 'vue-konva';
 
@@ -10,7 +10,7 @@ Vue.use(VueKonva)
 
 Vue.config.productionTip = false
 
-Vue.use(uweb, '1278161794')
+Vue.use(ba, 'd230bcea17546ecfcacf1b56c51cc8d8')
 
 new Vue({
   router,
